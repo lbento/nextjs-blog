75d356a51cbac07f5b66160ee69a77443aab9e4a
Lucas A. Bento <lbento@dcc.local>
2020-10-15 15:26:58 -0300

    remoção de esquema desnecessario

--- components/input/index.tsx
+++ components/input/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { useField } from '@unform/core';
-import { OutlinedInput , TextField } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
 
 export default function Input({ name, ...rest }) {
   const inputRef = useRef(null);
