0630e34a6b1931beae18e42ef6388984cd0f1d0f
Camila Mendes <mendemmcaila@gmail.com>
2020-10-15 00:19:56 -0300

    input

--- components/inputPassword/input.component.tsx
+++ components/inputPassword/input.component.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Input_, TextField_ } from './input.styles';
+import PasswordField from 'material-ui-password-field';
 
 interface InputCustomProps {
     placeholder: string;
 }
 
-const InputCustom : React.FC<InputCustomProps> = ({
+const InputCustom: React.FC<InputCustomProps> = ({
     placeholder
 }) => {
     return (
-        <TextField_ placeholder={placeholder} variant="outlined" label={placeholder} onChange={event => {
-            const { value } = event;
-
-        }}></TextField_>
+        <PasswordField hintText="Crie uma senha" floatingLabelText="Crie uma senha" errorText="Your password is too short" />
     )
 }
 
