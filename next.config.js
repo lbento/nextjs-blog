4a7aada58c3c1452446c0265cc19fd7b8de984df
Lucas A. Bento <lbento@dcc.local>
2020-10-16 18:02:51 -0300

    adicionando config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 const withPWA = require('next-pwa')
  
 module.exports = withPWA({
+    trailingSlash: true,
     pwa: {
         dest: 'public'
     }
 })
+
+module.exports = {
+    trailingSlash: true,
+  }
