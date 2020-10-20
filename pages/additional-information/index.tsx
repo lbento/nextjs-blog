ee8058d5670c7bd3a740aa5ec8ff2f7063fe3e9b
Lucas A. Bento <lbento@dcc.local>
2020-10-20 12:08:51 -0300

    inicio da validação de informações adicionais

--- pages/additional-information/index.tsx
+++ pages/additional-information/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import CustomCard from '../../components/customcard';
 import Input from '../../components/input';
 import Button from '../../components/formbutton';
 import Layout from '../../components/layout';
-import From from '../../components/form';
+import { Form } from '@unform/web';	
 import CustomSelect from '../../components/select';
 import CustomDialog from '../../components/dialog';
 import DatePicker from '../../components/datepicker';
@@ -14,9 +14,11 @@ import { SIGNUP } from '../../constants/api.constants';
 import { USER_ACCESS } from '../../constants/storage.constants';
 import { ISignup } from '../../interfaces/signup.interface';
 import { IUserAccess } from '../../interfaces/user-access.interface';
+import * as Yup from 'yup';
 
 const AdditionalInformation: React.FC<any> = () => {
   const router = useRouter();
+  const formRef = useRef(null);
 
   const [dialogData, setOpen] = React.useState({open: false, message: '', success: false});
 
@@ -34,29 +36,58 @@ const AdditionalInformation: React.FC<any> = () => {
   };
  
   const onsubmitHandler = async data => {
-
-    const userData: ISignup = {
-        email: router.query.email,
-        password: router.query.password,
-        name: data.name,
-        birthday: data.birthday,
-        nationality: data.nationality,
-        phone: data.phone,
-        gender: data.gender,
-        identity: {
-          type: 2,
-          number: data.cpf
+      try {
+        formRef.current.setErrors({});
+        const schema = Yup.object().shape({
+          name: Yup
+            .string()
+            .required('O nome é obrigatório'),
+          cpf: Yup
+            .string()
+            .matches(/^[0-9]*$/, 'O CPF deve conter apenas números')
+            .required('O CPF é obrigatório'),
+          phone: Yup
+            .string()
+            .matches(/^[0-9]*$/, 'O celular deve conter apenas números')
+            .required('O celular é obrigatório'), 
+        });
+  
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+  
+      const userData: ISignup = {
+          email: router.query.email,
+          password: router.query.password,
+          name: data.name,
+          birthday: data.birthday,
+          nationality: data.nationality,
+          phone: data.phone,
+          gender: data.gender,
+          identity: {
+            type: 2,
+            number: data.cpf
+          }
+        };
+  
+        try {
+            const res = await post<IUserAccess>(SIGNUP, { ...userData });
+            handleClickOpen("Parabéns, você foi cadastrado com sucesso!", true);
+            localStorage.setItem(USER_ACCESS, JSON.stringify(res))
         }
-      };
-
-    try {
-        const res = await post<IUserAccess>(SIGNUP, { ...userData });
-        handleClickOpen("Parabéns, você foi cadastrado com sucesso!", true);
-        localStorage.setItem(USER_ACCESS, JSON.stringify(res))
-    }
-    catch (error) {
-        handleClickOpen(error.message, false);
-    }
+        catch (error) {
+            handleClickOpen(error.message, false);
+        }
+      }
+      catch (err) {
+        const validationErrors = {};
+        if (err instanceof Yup.ValidationError) {
+          err.inner.forEach(error => {
+            validationErrors[error.path] = error.message;
+          });
+          formRef.current.setErrors(validationErrors);
+        }
+      }
   }
 
   const nationalitiesOptions = [
@@ -75,7 +106,7 @@ const AdditionalInformation: React.FC<any> = () => {
   return (
     <Layout>
             <CustomCard title={'Informações adicionais'} subheader={'Preencha as informações abaixo para se cadastrar'}>
-                <From onSubmit={onsubmitHandler}>
+                <Form onSubmit={onsubmitHandler} ref={formRef}>
                     <Input name="name" type="text" placeholder="Nome completo"/>
                     <Input name="cpf" type="text" placeholder="CPF"/>
                     <CustomSelect name="nationality" options={nationalitiesOptions} placeholder="Nacionalidade" />
@@ -84,7 +115,7 @@ const AdditionalInformation: React.FC<any> = () => {
                     <Input name="phone" type="text" placeholder="Celular"/>
                     <CustomCheckbox name="termsConditions">Eu aceito os <span style={{color: '#EC7000', textDecoration: 'underline'}}>Termos de Uso</span> e a <span style={{color: '#EC7000', textDecoration: 'underline'}}>Politica de Provacidade</span></CustomCheckbox>
                     <Button type={'submit'} disabled={false}>Continuar</Button>
-                </From>
+                </Form>
             </ CustomCard>
           <CustomDialog 
             open={dialogData.open}
